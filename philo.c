2bae3fc87fb174adfaaed91f46335ed062b09b9e
Anas Qadil <aqadil@e2r10p2.1337.ma>
2022-01-07 18:29:58 +0100

    .

--- philo.c
+++ philo.c
@@ -6,46 +6,16 @@
 /*   By: aqadil <aqadil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 16:59:36 by aqadil            #+#    #+#             */
-/*   Updated: 2022/01/07 18:19:33 by aqadil           ###   ########.fr       */
+/*   Updated: 2022/01/07 18:28:35 by aqadil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-pthread_mutex_t mutex;
-
-
-void	*my_turn(void *args)
+int main(int argc, char **argv)
 {
-	int j = 0;
-	int *i = (int *)args;
-	while (j < 10000000)
-	{
-		pthread_mutex_lock(&mutex);
-		(*i)++;
-		j++;
-		pthread_mutex_unlock(&mutex);
-	}
-	return (NULL);
-}
-
-
-int main()
-{
-	pthread_t	t1;
-	pthread_t	t2;
-	int	*arg;
-	int	i = 0;
-	pthread_mutex_init(&mutex, NULL);
+	if (argc != 4 || argc != 5)
+		return (1);
 	
-	
-	pthread_create(&t1, NULL, &my_turn, &i);
-	pthread_create(&t2, NULL, &my_turn, &i);
-	pthread_join(t1, NULL);
-	pthread_join(t2, NULL);
-
-	pthread_mutex_destroy(&mutex);
-	
-	printf("%d",i);
 	return (0);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: aqadil <aqadil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/01 17:30:50 by aqadil            #+#    #+#             */
-/*   Updated: 2022/01/01 17:31:41 by aqadil           ###   ########.fr       */
+/*   Updated: 2022/01/07 18:29:29 by aqadil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+
+typedef struct s_philo
+{
+    int philo_id;
+    
+}   t_philo;
 
 
 #endif
